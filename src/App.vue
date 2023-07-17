c60bcce9554308fbe454b509c15bf5df2c2dbb42
valeria caramellino <valeriacaramellino@gmail.com>
2023-07-17 16:06:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default{
+  data(){
+    return{
+
+    }
+   
+  }, 
+  components: {
+      AppMain
+    }
+}
+</script>
+
+<template>
+  <header>
+    testata
+  </header>
+    <AppMain />
+  <footer>
+    footer
+  </footer>
+ 
+</template>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+    name:"Appmail",
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+
+<template>
+<main>
+    <h3>corpo della pagina</h3>
+</main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
