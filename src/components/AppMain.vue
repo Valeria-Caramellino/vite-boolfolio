41d0947ffa427b97a14fc4e177973e5c322bc908
valeria caramellino <valeriacaramellino@gmail.com>
2023-07-17 19:58:57 +0200

    refactoring

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,60 +16,37 @@ export default{
     methods:{
         getProject(){
             this.loading = true;
+
             axios.get(this.apiUrl + this.projectApi).then((r) => {
-            /**metto i dati nell'array */
-            this.projects = r.data.results.data;
-            this.indexPageProjects = r.data.results.current_page;
-            this.totPageProjects = r.data.results.last_page;
-            //console.log(r.data.results.last_page);
-            //console.log(this.projects);
-            this.loading = false
-        }).catch(err=> {
-            this.loading = false;
-            this.loadingError = "Errore nel caricamento " + err.message;
-            console.error(err);
-        })
-        },
-        getProjectNextPage(){
-            if( this.indexPageProjects < this.totPageProjects ){
-                this.loading=true;
-                let config = {
-                    params: {
-                        page:(this.indexPageProjects + 1),
-                    }
-                };
-                axios.get(this.apiUrl + this.projectApi, config).then((r) => {
-                    /**metto i dati nell'array */
-                    this.projects = r.data.results.data;
-                    this.indexPageProjects = r.data.results.current_page;
-                    this.totPageProjects = r.data.results.last_page;
-                    //console.log(r.data.results.data);
-                    //console.log(this.projects);
-                    this.loading = false
-                }).catch(err=> {
-                    this.loading = false;
-                    this.loadingError = "Errore nel caricamento " + err.message;
-                    console.error(err);
-                })
-            }else {
-                console.error('non ci sono piu pagine');
-            }
             
-           // console.log("caricato seconda pg");
+                this.projects = r.data.results.data;/**metto i dati nell'array */
+                this.indexPageProjects = r.data.results.current_page;//popolo dati
+                this.totPageProjects = r.data.results.last_page;//popolo dati
+                //console.log(r.data.results.last_page);
+                //console.log(this.projects);
+                this.loading = false
+            }).catch(err=> {
+                this.loading = false;
+                this.loadingError = "Errore nel caricamento " + err.message;
+                //console.error(err);
+            })
         },
         getProjectsPage(PagNum){
-            if( PagNum <= this.totPageProjects ){
-                this.loading=true;
+
+            if( PagNum && PagNum > 0 && PagNum <= this.totPageProjects ){
+                
                 let config = {
                     params: {
                         page:PagNum,
                     }
                 };
+
+                this.loading=true;
                 axios.get(this.apiUrl + this.projectApi, config).then((r) => {
-                    /**metto i dati nell'array */
-                    this.projects = r.data.results.data;
-                    this.indexPageProjects = r.data.results.current_page;
-                    this.totPageProjects = r.data.results.last_page;
+                    
+                    this.projects = r.data.results.data;/**metto i dati nell'array */
+                    this.indexPageProjects = r.data.results.current_page;//popolo
+                    this.totPageProjects = r.data.results.last_page;//popolo dati
                     //console.log(r.data.results.data);
                     //console.log(this.projects);
                     this.loading = false
@@ -81,7 +58,14 @@ export default{
             }else {
                 console.error('non ci sono piu pagine');
             }
+        },
+        getProjectPrevPage(){
+            this.getProjectsPage(this.indexPageProjects - 1);
+        },
+        getProjectNextPage(){
+            this.getProjectsPage (this.indexPageProjects +1);
         }
+
 
     },
     mounted(){
@@ -111,7 +95,7 @@ export default{
 
     </div>
 
-    
+    <a class="btn btn-primary" @click="getProjectPrevPage">Pagina Prev</a>
     <a class="btn btn-primary mx-2" @click="getProjectsPage(numPage)" v-for="numPage in totPageProjects ">{{ numPage }}</a>
     <a class="btn btn-primary" @click="getProjectNextPage">Pagina next</a>
 </main>
