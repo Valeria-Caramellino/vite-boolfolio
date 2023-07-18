21b88fa1e53908651adc4423dfd2c40cb91bb08b
valeria caramellino <valeriacaramellino@gmail.com>
2023-07-18 15:47:28 +0200

    aggiunta variabili + creazione cartella style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.scss'
+import './style/General.scss'
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App.vue'
 import { router }  from './router'
